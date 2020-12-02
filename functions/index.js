7ab7abceb8739ce19c5e49d39528f1cf2a2f412f
Harrison <fayvorharrison101@gmail.com>
2020-12-02 15:40:40 +0100

    update hexcord email template

--- functions/index.js
+++ functions/index.js
@@ -22,29 +22,40 @@ const mailTransporter = nodemailer.createTransport({
 });
 
 const emailTemplate = ({ unsubscribeURI }) => `
-  <style>
-    .button__link {
-      border: none;
-      background: none;
-      padding: 0;
-      text-decoration: underline;
-      cursor: pointer;
-      outline: none;
-      color: #069;
-    }
-  </style>
-  <p>Hi there!</p>
-  <p>
-    So excited to have you onboard! We can't wait to have you try out Hexcord. As an early bird of course you get beta access.
-    How sweet is that? I knowwww.
-    Hexcord would be launching by the beginning of December and you can expect to be alerted the moment it is available.
-  </p>
-  <p>
-    If this was a mistake however and you didn't mean to sign up for beta access, it'll be sad :( to see you leave but you can unsubscribe 
-    <a href="${unsubscribeURI}" target="_blank">here.</a>
-  </p>
-  <p>Cheers!</p>
-  <p>Hexcord.</p>
+  <section style="margin: 0 5%;">
+    <header style="background: #0000FF;width: 100%;height: 78px;text-align: center;">
+      <span style="display: inline-block;height: 100%;vertical-align: middle;"></span>
+      <a style="text-decoration: none;" href="https://www.hexcord.com"><img alt="Hexcord Logo" style="vertical-align: middle;" src="https://res.cloudinary.com/hyper-debugger/image/upload/v1606919638/hexcord_logo.png" /></a>
+    </header>
+    <section style="background: #F8F8FF;">
+      <div style="width: 70%;margin: auto;">
+        <p style="font-weight:bold;font-family: Lato;font-style: normal;font-size: 16px;line-height: 26px;color: #080708;margin-bottom: 20px;">Welcome aboard!</p>
+        <p style="font-family: Lato;font-style: normal;font-size: 16px;line-height: 26px;color: #080708;margin-bottom: 20px;">
+          So excited to have you onboard! We can't wait to have you try out Hexcord.
+        </p>
+        <p style="font-family: Lato;font-style: normal;font-size: 16px;line-height: 26px;color: #080708;margin-bottom: 20px;">
+          As an early bird of course you get beta access.
+          How sweet is that? I knowwww!
+          Hexcord would be launching by the beginning of December and you can expect to be alerted the moment it is
+          available.
+        </p>
+        <p style="font-family: Lato;font-style: normal;font-size: 16px;line-height: 26px;color: #080708;margin-bottom: 20px;">Reply this email if you have any questions.</p>
+        <p style="font-family: Lato;font-style: normal;font-size: 16px;line-height: 26px;color: #080708;margin-bottom: 20px;">Cheers!</p>
+        <p style="font-family: Lato;font-style: normal;font-size: 16px;line-height: 26px;color: #080708;margin-bottom: 20px;">Hexcord.</p>
+        <footer style="border-top: 1px solid #E6E6FF;border-bottom: 1px solid #E6E6FF;padding-top: 20px;margin-bottom: 20px;">
+          <p style="font-family: Lato;font-style: normal;font-size: 14px;line-height: 23px;color: #424273;margin-bottom: 20px;">
+            If this was a mistake however and you didn't mean to sign up for beta access, it'll be sad :( to see you
+            leave
+            but you can unsubscribe
+            <a href="${unsubscribeURI}" target="_blank">here.</a>
+          </p>
+        </footer>
+        <a style="text-decoration: none;" href="https://twitter.com/thehexcord">
+          <span style="display: inline-block;width: 29px;height: 22.75px;background: url(https://res.cloudinary.com/hyper-debugger/image/upload/v1606919620/twitter_icon.png);"></span>
+        </a>
+      </div>
+    </section>
+  </section>
 `;
 
 const saveEmail = async (request, response) => {
@@ -67,7 +78,7 @@ const saveEmail = async (request, response) => {
         address: mailFrom
       },
       to: address,
-      subject: 'Welcome Aboard! You would be alerted as soon as the Hexcord service is up.',
+      subject: 'Hi there! Welcome Aboard.',
       html: emailTemplate({ unsubscribeURI }),
     };
 
